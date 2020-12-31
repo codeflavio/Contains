02b3f739302cb5500342e675651d5a9d1828bd47
codeflavio <flaviopvieira@hotmail.com>
2020-12-31 09:26:05 -0300

    contains

---
+++ Contains/EndsWith.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Contains
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string songLyrics = "you say goodbay, and y say hallo";
+            Console.WriteLine(songLyrics.EndsWith("você"));
+            Console.WriteLine(songLyrics.EndsWith("adeus"));
+        }
+    }
+}
